b6d4c65edbd5b3ec3ffa1eb17721748d6c566565
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-09-22 15:10:39 +0400

    config: add validation config variables.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,14 @@
 module.exports = {
   MONGODB: {
     CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING
+  },
+  VALIDATIONS: {
+    API_LIMIT_MAX_VALUE: 20,
+    OPTIONS: {
+      abortEarly: true,
+      allowUnknown: false,
+      convert: true
+    },
+    TOPICS: ['Trump', 'ISIS', 'Esports', 'Lady Gaga']
   }
 }
