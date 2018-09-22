78991c1977cf986553c16a130263167038b59ddb
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-09-22 14:02:40 +0400

    utils: modify errors util.

--- src/utils/errors.util.js
+++ src/utils/errors.util.js
@@ -1,3 +1,11 @@
+class InputValidationError extends Error {
+  constructor (message) {
+    super()
+    this.message = message
+    this.name = this.constructor.name
+  }
+}
+
 class PathNotFoundError extends Error {
   constructor (message) {
     super()
@@ -6,15 +14,8 @@ class PathNotFoundError extends Error {
   }
 }
 
-class ValidationError extends Error {
-  constructor (message) {
-    super()
-    this.message = message
-    this.name = this.constructor.name
-  }
-}
-
 module.exports = {
-  PathNotFoundError,
-  ValidationError
+  InputValidationError,
+  PathNotFoundError
+
 }
