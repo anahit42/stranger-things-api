797825283a3558140e0d66249814de725649e871
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-09-22 16:25:05 +0400

    api: attach statistics endpoints.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
+const statisticsApi = require('./statistics/statistics.api')
 const tweetsApi = require('./tweets/tweets.api')
 
+router.use('/statistics', statisticsApi)
 router.use('/tweets', tweetsApi)
 
 module.exports = router
